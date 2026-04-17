5f14e24bbf56f47d402e3c9295c036f915e984c7
YashGodbole1 <yash.godbole@entujn.com>
2026-04-17 18:26:19 +0530

    Added Swap 2 Numbers Without 3rd Variable

---
+++ Basics/SwapTwoNumbersWithoutUsingThirdVariable.cs
@@ -0,0 +1,10 @@
+// Swap Two Numbers (Without using third Variable)
+
+int a =10;
+b = 20;
+
+a= a+b;   // a = 10 + 20  i.e 30
+b= a-b;   // b = 30 - 20  i.e 10
+a= a-b;   // a = 30 - 10  i.e 20
+
+// now a = 20; b = 10;
