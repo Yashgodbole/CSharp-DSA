1317847dfae2d822ce75b361c811b82212e2a66a
YashGodbole1 <yash.godbole@entujn.com>
2026-04-17 18:39:07 +0530

    CheckNoIsPositiveNegativeZero

---
+++ Basics/CheckifNumberisPositiveNegativeZero.cs
@@ -0,0 +1,10 @@
+// Check if Number is Positive , Negative or Zero
+
+int num = -5;
+
+if (num > 0)
+    Console.WriteLine("Pos");
+else if (num < 0)
+    Console.WriteLine("Neg");
+else
+    Console.WriteLine("Zero");
