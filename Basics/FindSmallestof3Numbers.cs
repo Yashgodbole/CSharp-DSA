4a93c62643b61b7b87845b4663752cc54fe871b2
YashGodbole1 <yash.godbole@entujn.com>
2026-04-17 18:31:20 +0530

    Find Smallest of 3 Numbers

---
+++ Basics/FindSmallestof3Numbers.cs
@@ -0,0 +1,9 @@
+// Find Smallest of 3 Numbers 
+// it can handle equal condition 
+
+if (a <= b && a <= c)
+    Console.WriteLine(a);
+else if (b <= a && b <= c)
+    Console.WriteLine(b);
+else
+    Console.WriteLine(c);
