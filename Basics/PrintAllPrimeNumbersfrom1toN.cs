fbb24a21a549e5b291f4e3968d8c02d6f0a6e416
YashGodbole1 <yash.godbole@entujn.com>
2026-04-18 23:54:50 +0530

    PrintAllPrimeNumbersfrom1toN.cs

---
+++ Basics/PrintAllPrimeNumbersfrom1toN.cs
@@ -0,0 +1,17 @@
+// Print all prime numbers from 1 to 100
+
+
+for(int i=2; i<=100; i++){
+	bool isPrime = true;
+for(int i = 1; i<=num/2; i++)
+{
+	if(num % i==0)
+	{
+		 isPrime = false;
+		break;
+	}
+}
+
+if(isPrime)
+Console.WriteLine(n + " ");
+}
